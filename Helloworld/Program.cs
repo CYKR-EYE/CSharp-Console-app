34155d4e73d63228063313827f8c281688ed9227
CYKR <cykreye@gmail.com>
2025-03-28 22:46:21 +0100

    Ajout de la classe Voiture et de son utilisation
    Création d'une nouvelle classe `Voiture` avec des propriétés pour la marque, la couleur, le kilométrage, la motorisation et le prix. Ajout d'une instance de `Voiture` dans `Program.cs` et affichage de ses détails dans la console.

--- Helloworld/Program.cs
+++ Helloworld/Program.cs
@@ -1,1 +1,3 @@
-﻿
+﻿Voiture miata = new Voiture("Mazda", "rouge", 100000, "essence", 5000);
+
+Console.WriteLine($"La {miata.Marque} est de couleur {miata.couleur}, a {miata.kilometrage} km, est motorisée à l'{miata.motorisation} et coûte {miata.prix} euros.");
---
+++ Helloworld/VoitureModel.cs
@@ -0,0 +1,17 @@
+﻿class Voiture
+{
+    public string Marque;
+    public string couleur;
+    public int kilometrage;
+    public string motorisation;
+    public double prix;
+
+    public Voiture(string marque, string couleur, int kilometrage, string motorisation, double prix)
+    {
+        this.Marque = marque;
+        this.couleur = couleur;
+        this.kilometrage = kilometrage;
+        this.motorisation = motorisation;
+        this.prix = prix;
+    }
+}
